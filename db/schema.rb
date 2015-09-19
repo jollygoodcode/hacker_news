30d885cef522d944564796001ade44a78afb3696
Juanito Fatas <katehuang0320@gmail.com>
2015-09-20 01:48:12 +0800

    Create links table and link with title and url
         Failure/Error: click_on "submit"
         ActiveRecord::StatementInvalid:
           PG::UndefinedTable: ERROR:  relation "links" does not exist
           LINE 5:                WHERE a.attrelid = '"links"'::regclass
                                                     ^
           :               SELECT a.attname, format_type(a.atttypid,
    a.atttypmod),
                                pg_get_expr(d.adbin, d.adrelid),
    a.attnotnull, a.atttypid, a.atttypmod
                           FROM pg_attribute a LEFT JOIN pg_attrdef d
                             ON a.attrelid = d.adrelid AND a.attnum =
    d.adnum
                          WHERE a.attrelid = '"links"'::regclass
                            AND a.attnum > 0 AND NOT a.attisdropped
                          ORDER BY a.attnum

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150919174717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "links", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.string   "url",        null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
